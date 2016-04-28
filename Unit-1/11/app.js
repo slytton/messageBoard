b362be76a82a9abcbec404fa92b3cd5ceab1be67
Seth Lytton <slytton@gmail.com>
2016-04-28 14:53:55 -0600

    Add comment constructor to controller and a comment count to each post.

--- Unit-1/11/app.js
+++ Unit-1/11/app.js
@@ -1,26 +1,20 @@
 var app = angular.module('RedditClone', ['ngMessages']);
 
 app.controller('PostsController', function($scope){
-  $scope.vm = {};
-  $scope.vm.posts = [{title: 'Cool Birds',
-                      author: 'someguy',
-                      image:'http://www.gettyimages.ca/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg',
-                      description: "A very long description about nothing.",
-                      createdAt: new Date(),
-                      votes: 0},
-                     {title: 'Running Hampsters',
-                      author: 'Hampster Man',
-                      image:'http://cdn.theatlantic.com/assets/media/img/photo/2015/11/images-from-the-2016-sony-world-pho/s01_130921474920553591/main_900.jpg?1448476701',
-                      description: "A very long description about nothing.",
-                      createdAt: new Date(),
-                      votes: 0}];
 
-  var Post = function(title, author, image, description){
+  function Comment(author, text){
+    this.author = author;
+    this.text = text;
+    this.createdAt = Date.now();
+  }
+
+  function Post(title, author, image, description, comments){
     this.title = title;
     this.author = author;
     this.image = image;
     this.description = description;
-    this.createdAt = new Date();
+    this.createdAt = Date.now();
+    this.comments = comments || [];
     var votes = 0;
 
     this.getVotes = function(){
@@ -34,11 +28,27 @@ app.controller('PostsController', function($scope){
     }
   }
 
+
+  $scope.vm = {};
+  $scope.vm.posts = [ new Post('Cool Birds',
+                               'someguy',
+                               'http://www.gettyimages.ca/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg',
+                               "A very long description about nothing.",
+                               [new Comment('Bob', "Hi, my name is Bob"),
+                                new Comment('Steve, "Hi Bob, my name is Steve"')]),
+                      new Post('Running Hampsters',
+                               'Hampster Man',
+                               'http://cdn.theatlantic.com/assets/media/img/photo/2015/11/images-from-the-2016-sony-world-pho/s01_130921474920553591/main_900.jpg?1448476701',
+                               "A very long description about nothing.",
+                               [new Comment('Bruce', "I don't know what Bob and Steve are talking about.")])];
+
+
+
   $scope.addPost = function(){
     var newPost = new Post( $scope.vm.postsForm.title,
                             $scope.vm.postsForm.author,
                             $scope.vm.postsForm.image,
-                            $scope.vm.postsForm.description );
+                            $scope.vm.postsForm.description);
     $scope.vm.postsForm = {};
     $scope.vm.posts.push(newPost);
     $scope.addPostForm.$setPristine();
