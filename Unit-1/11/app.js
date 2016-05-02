66098d85d21fc8dc68977d338c82774465b6ee92
Seth Lytton <slytton@gmail.com>
2016-05-02 10:55:27 -0600

    Remove object constructors and refactor

--- Unit-1/11/app.js
+++ Unit-1/11/app.js
@@ -1,59 +1,45 @@
-var app = angular.module('RedditClone', ['ngMessages', 'angularMoment', 'ngAnimate']);
+var app = angular.module('RedditClone', ['ngAnimate', 'ngMessages', 'angularMoment']);
 
-app.controller('PostsController', function($scope){
+app.controller('PostsController', function($scope, $timeout){
 
-  function Comment(author, text){
-    this.author = author;
-    this.text = text;
-    this.createdAt = new Date();
-  }
 
-  function Post(id, title, author, image, description, comments){
-    this.getVotes = function(){
-      return this.votes;
-    }
-    this.upVote = function(){
-      this.votes++;
-    }
-    this.downVote = function(){
-      this.votes--;
-    }
-
-    this.addComment = function(comment){
-      this.comments.push(comment);
-    }
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.image = image;
-    this.description = description;
-    this.createdAt = new Date();
-    this.comments = comments || [];
-    this.votes = 0;
-  }
   $scope.vm = {};
   $scope.vm.showAddComments = false;
-  $scope.vm.posts = [ new Post(0,
-                               'Zany Birds',
-                               'someguy',
-                               'http://www.gettyimages.ca/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg',
-                               "A very long description about nothing.",
-                               [new Comment('Bob', "Hi, my name is Bob"),
-                                new Comment('Steve', "Hi Bob, my name is Steve")]),
-                      new Post(1,
-                               'Running Hampsters',
-                               'Hampster Man',
-                               'http://cdn.theatlantic.com/assets/media/img/photo/2015/11/images-from-the-2016-sony-world-pho/s01_130921474920553591/main_900.jpg?1448476701',
-                               "A very long description about nothing.",
-                               [new Comment('Bruce', "I don't know what Bob and Steve are talking about.")])];
 
+  $timeout(function(){
+    $scope.vm.posts = [ { id: 0,
+                          createdAt: new Date(),
+                          title:       'Zany Birds',
+                          author:      'someguy',
+                          image:       'http://www.gettyimages.ca/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg',
+                          description: "A very long description about nothing.",
+                          votes:       0,
+                          comments:    [{author: 'Bob', text:"Hi, my name is Bob", createdAt},
+                                        {author: 'Steve', text: "Hi Bob, my name is Steve"}]},
+                        { id: 1,
+                          createdAt: Date(),
+                          title:       'Running Hampsters',
+                          author:      'Hampster Man',
+                          image:       'http://cdn.theatlantic.com/assets/media/img/photo/2015/11/images-from-the-2016-sony-world-pho/s01_130921474920553591/main_900.jpg?1448476701',
+                          description: "A very long description about nothing.",
+                          votes:       0,
+                          comments:    [{author: 'Bruce', text: "I don't know what Bob and Steve are talking about."}]},
+                        { id: 2,
+                          createdAt: Date(),
+                          title:       'Basket Man',
+                          author:      'Basket Man',
+                          image:       'http://im.rediff.com/news/2015/dec/24tpoty20.jpg',
+                          description: "Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles ",
+                          votes:       0,
+                          comments:    [{author: 'Bruce', text: "I don't know what Bob and Steve are talking about."}]}];
+  })
 
   $scope.addPost = function(){
-    var newPost = new Post( $scope.vm.posts.length,
-                            $scope.vm.postsForm.title,
-                            $scope.vm.postsForm.author,
-                            $scope.vm.postsForm.image,
-                            $scope.vm.postsForm.description);
+    var newPost = angular.copy($scope.vm.postsForm);
+    newPost.id = $scope.vm.posts.length,
+    newPost.createdAt = new Date();
+    newPost.comments = [];
+
     $scope.vm.postsForm = {};
     $scope.vm.posts.push(newPost);
     $scope.addPostForm.$setPristine();
@@ -76,8 +62,9 @@ app.controller('PostsController', function($scope){
       return delete subScope.commentsForm[key];
     })
 
-    post.comments.push(new Comment(copiedComment.author,
-                                   copiedComment.comment));
+    copiedComment.createdAt = new Date();
+
+    post.comments.push(copiedComment);
 
     subScope.addCommentForm.$setPristine();
     subScope.showComments = true;
