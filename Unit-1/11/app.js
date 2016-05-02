a44fb77289f48675743d9789956efc06ef65b340
Seth Lytton <slytton@gmail.com>
2016-05-02 12:11:01 -0600

    improve seed data

--- Unit-1/11/app.js
+++ Unit-1/11/app.js
@@ -8,30 +8,30 @@ app.controller('PostsController', function($scope, $timeout){
 
   $timeout(function(){
     $scope.vm.posts = [ { id: 0,
-                          createdAt: new Date(),
+                          createdAt:   new Date('04/30/2016'),
                           title:       'Zany Birds',
                           author:      'someguy',
                           image:       'http://www.gettyimages.ca/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg',
                           description: "A very long description about nothing.",
                           votes:       0,
-                          comments:    [{author: 'Bob', text:"Hi, my name is Bob", createdAt},
-                                        {author: 'Steve', text: "Hi Bob, my name is Steve"}]},
+                          comments:    [{author: 'Bob', text:"Hi, my name is Bob", createdAt: new Date('04/30/2016')},
+                                        {author: 'Steve', text: "Hi Bob, my name is Steve", createdAt: new Date('04/30/2016')}]},
                         { id: 1,
-                          createdAt: Date(),
+                          createdAt:   new Date(),
                           title:       'Running Hampsters',
                           author:      'Hampster Man',
                           image:       'http://cdn.theatlantic.com/assets/media/img/photo/2015/11/images-from-the-2016-sony-world-pho/s01_130921474920553591/main_900.jpg?1448476701',
                           description: "A very long description about nothing.",
                           votes:       0,
-                          comments:    [{author: 'Bruce', text: "I don't know what Bob and Steve are talking about."}]},
+                          comments:    [{author: 'Bruce', text: "I don't know what Bob and Steve are talking about.", createdAt: new Date()}]},
                         { id: 2,
-                          createdAt: Date(),
+                          createdAt:   new Date(),
                           title:       'Basket Man',
                           author:      'Basket Man',
                           image:       'http://im.rediff.com/news/2015/dec/24tpoty20.jpg',
                           description: "Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles Bicycles ",
                           votes:       0,
-                          comments:    [{author: 'Bruce', text: "I don't know what Bob and Steve are talking about."}]}];
+                          comments:    [{author: 'Bruce', text: "I don't know what Bob and Steve are talking about.", createdAt: new Date()}]}];
   })
 
   $scope.addPost = function(){
